c0204c55bdf2cf4b33662e84089dd304896f519f
agustin-kiryk <agustin.kiryk@gmail.com>
2023-03-24 11:08:06 -0300

    Create Transactions Entity and relationship of client and costumes

--- Backend/appDisfraces/src/main/java/com/dsi/appDisfraces/entity/CostumeEntity.java
+++ Backend/appDisfraces/src/main/java/com/dsi/appDisfraces/entity/CostumeEntity.java
@@ -1,14 +1,17 @@
 package com.dsi.appDisfraces.entity;
 
 import com.dsi.appDisfraces.enumeration.CostumeStatus;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.ArrayList;
 import java.util.Date;
@@ -65,7 +68,9 @@ public class CostumeEntity {
   @Column ( name= "Color")
   private String colour;
 
-
+  @OneToMany(mappedBy = "costume", fetch = FetchType.LAZY,
+      cascade = CascadeType.ALL) //TODO: Revisar que la cascade ande ok.
+  private List<TransactionEntity> transactions = new ArrayList<>();
 
 
 
---
+++ Backend/appDisfraces/src/main/java/com/dsi/appDisfraces/entity/TransactionEntity.java
@@ -0,0 +1,62 @@
+package com.dsi.appDisfraces.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Transacciones")
+public class TransactionEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "Cliente_id", referencedColumnName = "id")
+  private ClientEntity client;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "Disfraz_id", referencedColumnName = "id")
+  private ClientEntity costume;
+
+  @Column(name = "Fecha_de_alquiler")
+  private Date rentDate;
+
+  @Column(name = "Fecha_de_devolucion", nullable = true)
+  private Date deadline;
+
+  @Column(name = "Monto")
+  private Double ammount;
+
+  @Column(name = "Tipo_de_pago")
+  private String type;
+
+  @Column(name = "REMITO")
+  private String remito;
+
+  @Column(name = "Fecha_pago")
+  private Date date;
+
+
+
+
+
+
+
+
+
+
+}
